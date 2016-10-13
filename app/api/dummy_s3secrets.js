28737b7ff47333b3ed4910a6e81b6f2436af27bf
himanshu.jariyal <himanshujariyal@gmail.com>
2016-10-14 01:07:50 +0530

    dummy s3 secrets file

---
+++ app/api/dummy_s3secrets.js
@@ -0,0 +1,11 @@
+// Copy contents and create a s3secrets.js file inside api/
+
+export const data = {
+  accessKeyId: '',
+  secretAccessKey: '',
+  region: 'ap-south-1',
+};
+
+export default {
+  data,
+};
